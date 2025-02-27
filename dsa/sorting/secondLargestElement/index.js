67e72a6ff017bd0195c317c810713be5a203c8c7
aryapratap.maurya20 <aryapratap.maurya20@gmail.com>
2025-02-27 14:40:51 +0000

    added necessary comments

--- dsa/sorting/secondLargestElement/index.js
+++ dsa/sorting/secondLargestElement/index.js
@@ -1,13 +1,23 @@
+/**
+ * ------- Why let secondLargest = -1? ------------
+ * Acts as a Placeholder:
+ * 
+ * Since arrays typically contain positive numbers,
+ * -1 is a safe default to indicate that no valid 
+ * second-largest number has been found yet.
+ * 
+ **/
+
+
 const numArr = [1, 2, 3, 4, 5, 6, 6, 7];
 
 // find second largest element in an array - may contain repeared values
 function secondlargestfn(arr) {
 
     // base condition
-    if (arr.length < 2) {
-        return -1;
-    }
+    if (arr.length < 2) return -1;
 
+    // main logic
     let largest = arr[0]; // assume first element as largest
     let secondLargest = -1; // initialize second largest element as it do no exist initially
 
@@ -17,7 +27,7 @@ function secondlargestfn(arr) {
             largest = arr[i]; // update largest element
         } else if (
             // check if current element is greater than second largest and less than largest
-            arr[i] > secondLargest && 
+            arr[i] > secondLargest &&
             arr[i] < largest
         ) {
             secondLargest = arr[i];
