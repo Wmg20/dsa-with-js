48eccf244ccda186f4eebc68a481f7513f467b7e
aryapratap.maurya20 <aryapratap.maurya20@gmail.com>
2025-02-28 18:45:14 +0000

    rotate an array to left

---
+++ dsa/sorting/rotateAnArray/index.js
@@ -0,0 +1,26 @@
+// rotate an array by on place
+const arr = [1,2,3,4,5,6]
+
+function rotateArr (arr) {
+
+    // base case
+    if (arr.length <= 1) {
+        return arr;
+    }
+
+
+    let n = arr.length; 
+    let temp = arr[0]; // store first element
+
+    for (let i = 1; i < n; i++) {
+        // shift element of array by one
+        arr[i-1] = arr[i] 
+    }
+
+    // assign last element to first
+    arr[n-1] = temp;
+
+    return arr;
+}
+
+console.log(rotateArr(arr))
