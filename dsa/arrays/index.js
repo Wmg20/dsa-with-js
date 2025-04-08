a8a40dbaaf9ecdc6b31b12796ebeb5ca41ec2b11
aryapratap.maurya20 <aryapratap.maurya20@gmail.com>
2025-04-08 16:24:46 +0000

    Moving zeroes at the end in an array

--- dsa/arrays/index.js
+++ dsa/arrays/index.js
@@ -1,3 +1,34 @@
+// Suggested code may be subject to a license. Learn more: ~LicenseLog:341767011.
+// Suggested code may be subject to a license. Learn more: ~LicenseLog:3748272145.
 /**
  * Arrays
- */
+*/
+
+// 1. Moving all the zero in array at the end
+
+const arr = [1,4,0,3,2,0,9,0]
+
+function moveZeroAtEnd (arr) {
+   if (arr.length === 0) return [];
+   
+   let nonZeroIndex = 0;
+ 
+   // Iterate through the array
+   for (let i = 0; i < arr.length; i++) {
+     // If the current element is non-zero
+     if (arr[i] !== 0) {
+       // Move it to the next non-zero index
+       arr[nonZeroIndex] = arr[i];
+       nonZeroIndex++;
+     }
+   }
+ 
+   // Fill the rest of the array with zeros
+   for (let i = nonZeroIndex; i < arr.length; i++) {
+     arr[i] = 0;
+      }
+   return arr
+}
+console.log(moveZeroAtEnd(arr))
+
+
---
+++ js/event-loop/index.js
@@ -0,0 +1,12 @@
+/**
+ * The event loop is a crucial component of JavaScript's concurrency model.
+ * It continuously monitors the call stack and the callback queue.
+ * When the call stack is empty, it moves the first callback from the queue to the stack for execution.
+ * This enables asynchronous behavior, allowing JavaScript to handle tasks without blocking.
+ * 
+ */
+
+
+
+
+ 
