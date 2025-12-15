a9f1cb9a35f262820f9c29b93957ad78cf964e69
aryapratap.maurya20 <aryapratap.maurya20@gmail.com>
2025-12-15 03:46:15 +0000

    feat: Add addAtIndex method to MyLinkedList

--- dsa/linkedList/index.js
+++ dsa/linkedList/index.js
@@ -3,7 +3,7 @@
 
 /**
  * --------- new keyword -----------------
- * new is used to create an instance (object) from a constructor function or class.
+ *  new is used to create an instance (object) from a constructor function or class.
  * 
  *  const obj = new Constructor();
  * 
@@ -15,7 +15,12 @@
  */
 
 
-// Create a node
+/**
+ * ----- Create a node -----
+ *  A node contains two parameters
+ *  1. value
+ *  2. next - points to new node - refrence
+ */ 
 function MyNode (val) {
     this.val = val;
     this.next = null;
@@ -37,11 +42,18 @@ MyLinkedList.prototype.addAtHead = function (val) {
     this.size++;
 }
 
+/**
+ * ----------- STEPS ----------
+ * 1. Create new node
+ * 2. reach last node
+ * 3. link new node at the last
+ * 4. handle corner cases
+ * 5. increae the sie - size++
+ */
 MyLinkedList.prototype.addAtTail = function (val) {
     var newNode = new MyNode(val);
 
     if (!this.head) {
-        
         this.head = newNode;
     } else {
         var current = this.head;
@@ -55,12 +67,38 @@ MyLinkedList.prototype.addAtTail = function (val) {
     this.size++;
 }
 
+MyLinkedList.prototype.addAtIndex = function (val, index) {
+    const newNode = new MyNode(val);
+
+    if (index === 0) {
+        this.addAtHead(val);
+        return;
+    } else if (index === this.size) {
+        this.addAtTail(val)
+        return;
+    } else {
+        let cur = this.head;
+
+        for(let i = 0; i < index - 1; i++) {
+            cur = cur.next;
+        }
+
+        // first connect newNode next with the currents next node
+        // because it will lost if we try to add the node at head first
+        newNode.next = cur.next;
+        cur.next = newNode;
+       
+    }
+
+    this.size++;
+}
+
 // add a node to MyLinkedList
 const list = new MyLinkedList();
-// list.addAtHead(5);
+list.addAtHead(5);
 list.addAtHead(6);
 list.addAtTail(7);
 
-console.log(list);
+list.addAtIndex(11, 1)
 
-// Add at head
+console.log(list);
